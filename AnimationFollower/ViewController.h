ec0075e223e90f8acb9f35d20652ae49fd38fe1e
李刚 <LeeG4ng@qq.com>
2017-10-31 12:32:52 +0800

    Initial Commit

---
+++ AnimationFollower/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationFollower
+//
+//  Created by Gary Lee on 2017/10/31.
+//  Copyright © 2017年 LeeG4ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AnimationFollower/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AnimationFollower
+//
+//  Created by Gary Lee on 2017/10/31.
+//  Copyright © 2017年 LeeG4ng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
